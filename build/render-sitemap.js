0f00ae25a100354a941c79a52b2fe27289976827
nikulinri-lang <nikulinri@gmail.com>
2026-09-24 15:40:11 +0300

    SEO: fix canonical sitemap URLs for catalog and blog

--- build/render-sitemap.js
+++ build/render-sitemap.js
@@ -14,14 +14,14 @@ function render(distDir, { products, blogPosts }) {
 
   const urls = [
     { loc: "/", lastmod: buildDate },
-    { loc: "/catalog", lastmod: buildDate },
+    { loc: "/catalog/", lastmod: buildDate },
     { loc: "/printer", lastmod: buildDate },
     { loc: "/about", lastmod: buildDate },
     { loc: "/delivery", lastmod: buildDate },
     { loc: "/custom-order", lastmod: buildDate },
     { loc: "/offer", lastmod: buildDate },
     { loc: "/privacy", lastmod: buildDate },
-    { loc: "/blog", lastmod: buildDate },
+    { loc: "/blog/", lastmod: buildDate },
     ...products.map((p) => ({ loc: `/catalog/${p.slug}`, lastmod: buildDate })),
     ...blogPosts.map((p) => ({ loc: `/blog/${p.slug}`, lastmod: new Date(p.date).toISOString().slice(0, 10) })),
   ];
